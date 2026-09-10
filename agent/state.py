618dbb2b2a2eb47cf55857cf6fd26eff88e1e84b
UzairAtiq <uzairatiq65@gmail.com>
2026-09-10 17:34:19 +0500

    feat: define MessagesState with add_messages reducer

--- agent/state.py
+++ agent/state.py
@@ -1,2 +1,10 @@
+from langchain.messages import AnyMessage
+from typing_extensions import TypedDict, Annotated
+import operator
+
+#Defininf the message state manually 
+class MessagesState(TypedDict):
+    messages: Annotated[list[AnyMessage], operator.add]
+    llm_calls: int
 
 
