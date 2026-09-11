bfe7fdddfc9e743d6e08fa8c8c93eba0adb97f95
UzairAtiq <uzairatiq65@gmail.com>
2026-09-11 13:45:19 +0500

    fix: fix node registration and edge wiring in graph.py

---
+++ agent/graph.py
@@ -0,0 +1,41 @@
+from agent.state import MessagesState
+from langgraph.graph import StateGraph,START,END
+from agent.nodes import agent_node
+from langgraph.prebuilt import ToolNode
+from agent.nodes import get_tools
+from agent.nodes import should_continue
+from agent.nodes import bind_llm_with_tools
+from agent.nodes import make_agent_node
+from IPython.display import Image, display
+
+
+#getting the tools 
+tools = get_tools()
+
+#binding the tools with the llm 
+llm_with_tools = bind_llm_with_tools(tools)
+
+#creating the agent
+agent_node = make_agent_node(llm_with_tools)
+
+
+agent_builder = StateGraph(MessagesState) 
+
+#Adding nodes 
+agent_builder.add_node("agent_node" , agent_node() )
+
+agent_builder.add_node("tool_node" , ToolNode(tools) )
+
+# Add edges to connect nodes
+agent_builder.add_edge(START, "agent_node")
+agent_builder.add_conditional_edges(
+    "agent_node",
+    should_continue,
+    ["tool_node", END]
+)
+agent_builder.add_edge("tool_node", "agent_node")
+
+# Compile the agent
+agent = agent_builder.compile()
+
+display(Image(agent.get_graph(xray=True).draw_mermaid_png()))
