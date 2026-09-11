c390b7caeae975f68fc8e27d5b7fc41d89261ef2
UzairAtiq <uzairatiq65@gmail.com>
2026-09-11 14:33:47 +0500

    fix: use add_messages reducer instead of operator.add for MessagesState

--- agent/state.py
+++ agent/state.py
@@ -1,10 +1,11 @@
 from langchain.messages import AnyMessage
+from langgraph.graph.message import add_messages
 from typing_extensions import TypedDict, Annotated
 import operator
 
 #Defininf the message state manually 
 class MessagesState(TypedDict):
-    messages: Annotated[list[AnyMessage], operator.add]
+    messages: Annotated[list[AnyMessage], add_messages]
     llm_calls: int
 
 
